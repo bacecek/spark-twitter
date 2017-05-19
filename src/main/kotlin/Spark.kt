82bef21f248f92c2a94d650b4fe2fd1e0c88c68f
Denis Buzmakov <lysva94@gmail.com>
2017-05-20 00:14:01 +0500

    init

---
+++ src/main/kotlin/Spark.kt
@@ -0,0 +1,41 @@
+import org.apache.hadoop.mapred.InvalidInputException
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+import scala.Tuple2
+import java.io.IOException
+
+fun main(args: Array<String>) {
+
+    // Получение имени файла, в котором хранятся твиты
+    val inputFile = args[0]
+
+    // Инициализация Spark
+    val config = SparkConf()
+            .setMaster("local")
+            .setAppName("Spark")
+    val sparkContext = JavaSparkContext(config)
+
+    try {
+        // Считывание текста из файла
+        val text = sparkContext.textFile(inputFile)
+        // Получение списка слов
+        val words = text.flatMap { it -> it.replace("\\pP\\s+".toRegex(), "").toLowerCase().split(" ") }
+
+        // Получение результата - списка слов и их количество в твитах
+        val counts = words
+                .mapToPair { x -> Tuple2(x, 1) }
+                .reduceByKey { x, y -> x + y }
+                .filter { x -> x._2() > 10 }
+                .collect()
+                .sortedByDescending { it._2() }
+
+        // Вывод в консоль
+        for (count in counts) {
+            println("${count._1()} ${count._2()}")
+        }
+    } catch(e: InvalidInputException) {
+        println("нет такого файла!")
+    } catch(e: IOException) {
+        println("проверьте правильность установки Hadoop и Spark на свой компьютер")
+    }
+}
